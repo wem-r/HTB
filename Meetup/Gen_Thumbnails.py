7861e5fb77fd92410ffe45673ed6371a9784219a
wem-r <57370095+wem-r@users.noreply.github.com>
2023-02-07 23:35:43 +0100

    updated background.png, adjusted box igm positions

--- Meetup/Gen_Thumbnails.py
+++ Meetup/Gen_Thumbnails.py
@@ -64,13 +64,13 @@ width = 2560
 height = 1440
 background = Image.open('background.png')
 
-font1 = ImageFont.truetype("Zeitung_Micro_Pro.ttf", 190)
+font1 = ImageFont.truetype("Zeitung_Micro_Pro.ttf", 170)
 font2 = ImageFont.truetype("Zeitung_Micro_Pro.ttf", 80)
 
-meetup = f"OSCP-Like {args.num_meetup}"
+meetup = f"Meetup HTB France {args.num_meetup}"
 draw = ImageDraw.Draw(background)
 w, h = draw.textsize(meetup, font=font1)
-draw.text(((width-w)/2, 50), meetup, (255,255,255), font=font1)
+draw.text(((width-w)/2, 80), meetup, (20,29,44), font=font1)
 
 #-------------------------------------------------------------
 # Box
@@ -85,22 +85,22 @@ if args.machine3 != None:
     im3 = Image.open(requests.get(base_url + m3_avatar, headers=headers, stream=True).raw)
 
 if len(sys.argv)==5:
-    draw.text(((width-w1)/2, 900), machine1, (255,255,255), font=font2)
-    background.paste(im1, (int((width-300)/2), 570), im1)
+    draw.text(((width-w1)/2, 925), machine1, (255,255,255), font=font2)
+    background.paste(im1, (int((width-300)/2), 625), im1)
 
 if len(sys.argv)==7:
-    draw.text((800 - (w1/2), 900), machine1, (255,255,255), font=font2)
-    background.paste(im1, (650, 570), im1)
-    draw.text((1760 - (w2/2), 900), machine2, (255,255,255), font=font2)
-    background.paste(im2, (1605, 570), im2)
+    draw.text((800 - (w1/2), 925), machine1, (255,255,255), font=font2)
+    background.paste(im1, (650, 625), im1)
+    draw.text((1760 - (w2/2), 925), machine2, (255,255,255), font=font2)
+    background.paste(im2, (1605, 625), im2)
 
 if len(sys.argv)==9:
-    draw.text((565 - (w1/2), 830), machine1, (255,255,255), font=font2)
-    background.paste(im1, (415, 500), im1)
-    draw.text((1280 - (w2/2), 830), machine2, (255,255,255), font=font2)
-    background.paste(im2, (1130, 500), im2)
-    draw.text((1995 - (w3/2), 830), machine3, (255,255,255), font=font2)
-    background.paste(im3, (1845, 500), im3)
+    draw.text((565 - (w1/2), 925), machine1, (255,255,255), font=font2)
+    background.paste(im1, (415, 625), im1)
+    draw.text((1280 - (w2/2), 925), machine2, (255,255,255), font=font2)
+    background.paste(im2, (1130, 625), im2)
+    draw.text((1995 - (w3/2), 925), machine3, (255,255,255), font=font2)
+    background.paste(im3, (1845, 625), im3)
 
 #-------------------------------------------------------------
 background.save(f'{args.num_meetup}.png')
